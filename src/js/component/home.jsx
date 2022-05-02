c6e495227dfd8639477bf6b1564bf395e4e87198
Chris-Durr <chrisdurr88@gmail.com>
2022-05-02 09:34:45 +0000

    Lista terminada

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,102 +1,51 @@
-import React, { useState } from "react";
-import { Button, Card, Form } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-function Todo({ valorTodo, index, completado, borrar }) {
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
+
+export default function Home() {
+	const [tarea, setTarea] = useState("");
+	const [lista, setLista] = useState([]);
+
+	function borrar(i) {
+		let re = lista.filter((valor, index) => {
+			return index != i;
+		});
+		setLista(re);
+	}
 	return (
-		<div className="todo">
-			<span
-				style={{
-					textDecoration: valorTodo.isDone ? "line-through" : "",
+		<div className="ToDoApp text-center">
+			<h1>ToDO List</h1>
+			<input
+				placeholder="Add ToDo"
+				onChange={(e) => {
+					setTarea(e.target.value);
+				}}
+			/>
+			<button
+				onClick={() => {
+					setLista([...lista, tarea]);
 				}}>
-				{valorTodo.text}
-			</span>
-			<div>
-				<Button
-					variant="outline-success"
-					onClick={() => completado(index)}>
-					✓
-				</Button>{" "}
-				<Button variant="outline-danger" onClick={() => borrar(index)}>
-					✕
-				</Button>
-			</div>
+				Agregar ToDO
+			</button>
+			<ul id="uladd">
+				{lista.map(function (valor, i) {
+					return (
+						<li key={i}>
+							{valor}
+							<Button
+								variant="outline-danger"
+								onClick={() => {
+									borrar(i);
+								}}>
+								✕
+							</Button>
+						</li>
+					);
+				})}
+			</ul>
 		</div>
 	);
 }
-
-function FormTodo({ addTodo }) {
-	const [value, setValue] = useState("");
-
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		if (!value) return;
-		addTodo(value);
-		setValue("");
-	};
-
-	return (
-		<Form className="text-center" id="form" onSubmit={handleSubmit}>
-			<Form.Group>
-				<Form.Label>Add something to do!</Form.Label>
-				<Form.Control
-					type="text"
-					className="input"
-					value={value}
-					onChange={(e) => setValue(e.target.value)}
-					placeholder="What do you wanna do?"
-				/>
-			</Form.Group>
-			<Button id="sendbutton" variant="primary mb-3" type="submit">
-				Add to the list
-			</Button>
-		</Form>
-	);
-}
-
-function App() {
-	const [lista, setlista] = useState([]);
-
-	const addTodo = (text) => {
-		const nuevalista = [...lista, { text }];
-		setlista(nuevalista);
-	};
-
-	const completado = (index) => {
-		const nuevalista = [...lista];
-		nuevalista[index].isDone = true;
-		setlista(nuevalista);
-	};
-
-	const borrar = (index) => {
-		const nuevalista = [...lista];
-		nuevalista.splice(index, 1);
-		setlista(nuevalista);
-	};
-
-	return (
-		<div className="app">
-			<div className="container">
-				<h1 className="text-center mb-4">To-do List</h1>
-				<FormTodo addTodo={addTodo} />
-				<div>
-					{lista.map((valorTodo, index) => (
-						<Card className="text-center" id="card">
-							<Card.Body>
-								<Todo
-									key={index}
-									index={index}
-									valorTodo={valorTodo}
-									completado={completado}
-									borrar={borrar}
-								/>
-							</Card.Body>
-						</Card>
-					))}
-				</div>
-			</div>
-		</div>
-	);
-}
-
-export default App;
